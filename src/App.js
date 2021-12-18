2fe47486a99683e275681050f21b98d4857a01dc
Gaurav <gauravpawar3102@gmail.com>
2021-12-18 15:26:28 +0530

    Css added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
+import Header from './components/Header';
+
 function App() {
-  const title = 'Blog post';
-  const body = 'This is my blog post';
-  const comments = [
-    { id: 1, text: 'comment one' },
-    { id: 2, text: 'comment two' },
-    { id: 3, text: 'comment three' },
-  ];
-
   return (
-    <div className="container">
-      <h1>{title.toLocaleUpperCase()}</h1>
-      <p>{body.toLocaleLowerCase()}</p>
-
-      <div className="comments">
-        <h3>Comments({comments.length})</h3>
-        <ul>
-          {comments.map((comment, index) => (
-            <li key={index}>{comment.text}</li>
-          ))}
-        </ul>
+    <>
+      <Header text="Namaste Javascript" />
+      <div className="container">
+        <h1>My App</h1>
       </div>
-    </div>
+    </>
   );
 }
 export default App;
