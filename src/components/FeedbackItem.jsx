8d547a5cd5d2586ccada9096066f71e5bcb9aa75
Gaurav <gauravpawar3102@gmail.com>
2021-12-24 16:36:51 +0530

    usestate

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 
 function FeedbackItem() {
-  const [rating, setRating] = useState(5);
+  const [rating, setRating] = useState(7);
   const [text, setText] = useState('This is an example of an feedback item');
 
   return (
